66d9ff3a73f2c79983b60817310aa75d5136a24e
Paulo Victor <paulovictorlima418@gmail.com>
2023-10-26 00:09:56 -0300

    Estrutura da aplicação - Rotas de criação e listagem (Métodos HTTP)

--- src/server.js
+++ src/server.js
@@ -2,9 +2,24 @@
 //CommonJS => require -> obsoleto
 
 //ESModules => import/export
-import http from 'node:http' //node: por ser módulo interno
+import http, { get } from 'node:http' //node: por ser módulo interno
+//GET => Buscar um recurso no back-end
+//POST => Criar um recurso no back-end
+//PUT => Atualizar um recurso no back-end
+//PATCH => Atualizar uma informação específica de um recurso no back-end
+//DELETE => Deletar um recurso no back-end
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(function(req, res) {
+  const { method, url } = req  //const method = req.method
+
+  if(method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários')
+  }
+  
+  if(method === 'POST' && url === '/users') {
+    return res.end('Criação de usuários')
+  }
+
   return res.end('Hello World')
 })
 
