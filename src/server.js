8a7396361f1d01792469ad0088a3918e0441d1ca
Paulo Victor <paulovictorlima418@gmail.com>
2023-10-30 21:28:08 -0300

    Estrutura da aplicação - Salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -9,14 +9,30 @@ import http, { get } from 'node:http' //node: por ser módulo interno
 //PATCH => Atualizar uma informação específica de um recurso no back-end
 //DELETE => Deletar um recurso no back-end
 
+//Statefull (info salva na memoria) - Stateless (N salva em memoria - só em databate etc, é o ideal)
+
+//JSON (javascript object notation - transitar diferentes tipos de dados em strings)
+
+//Cabeçalhos (req/res) => Metadados
+
+const users = []
+
 const server = http.createServer(function(req, res) {
   const { method, url } = req  //const method = req.method
 
   if(method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    return res
+    .setHeader('Content-type', 'application/json')//setar no cabeçalho o tipo de dado especifico
+    .end(JSON.stringify(users))
   }
   
   if(method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    })
+
     return res.end('Criação de usuários')
   }
 
